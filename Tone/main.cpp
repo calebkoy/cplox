26e479c2dcc0477603dde36c04bfc514a3253750
Caleb Owusu-Yianoma <caleb_owusuyianoma@hotmail.co.uk>
2020-09-22 23:20:24 +0100

    create main and gitignore

---
+++ Tone/main.cpp
@@ -0,0 +1,16 @@
+void repl() {
+
+}
+
+void runFile() {
+}
+
+int main(int argc, char *argv[]) {
+    if (argc == 0) {
+        repl();
+    } else if (argc == 1) {
+        runFile();
+    }
+
+    return 0;
+}
