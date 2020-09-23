2d5dff4bc062e452f9138c359055ede51901e2b6
Caleb Owusu-Yianoma <caleb_owusuyianoma@hotmail.co.uk>
2020-09-23 11:26:02 +0100

    enable reading of text from file and modify gitignore

--- Tone/main.cpp
+++ Tone/main.cpp
@@ -1,8 +1,10 @@
 #include <cstdio>
+#include <fstream>
 #include <iostream>
+#include <string>
 
 void repl() {
-  char line[1024];
+  char line[1024]; // Q: Could I use std::string?
   for (;;) {
     std::cout << ">> ";
 
@@ -15,15 +17,26 @@ void repl() {
   }
 }
 
-void runFile() {
-  std::cout << "runFile()\n"; // Temp
+void runFile(const char *path) {
+  std::ifstream inputFile{ path };
+
+  if (!inputFile) {
+    std::cerr << "Cannot open " << path << " for reading.\n";
+    exit(66);
+  }
+
+  while (inputFile) {
+    std::string fileText;
+    std::getline(inputFile, fileText);
+    std::cout << fileText << '\n';
+  }
 }
 
 int main(int argc, char *argv[]) {
   if (argc == 1) {
     repl();
   } else if (argc == 2) {
-    runFile();
+    runFile(argv[1]);
   } else {
     // Print an error, telling the user the expected usage
     // Exit w/ an exit code
