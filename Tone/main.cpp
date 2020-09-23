e6c210acc33220f20500f7ef51f6fa5cd7305b6c
Caleb Owusu-Yianoma <caleb_owusuyianoma@hotmail.co.uk>
2020-09-23 10:55:15 +0100

    create skeleton repl function

--- Tone/main.cpp
+++ Tone/main.cpp
@@ -1,16 +1,33 @@
-void repl() {
+#include <cstdio>
+#include <iostream>
 
+void repl() {
+  char line[1024];
+  for (;;) {
+    std::cout << ">> ";
+
+    if (!fgets(line, sizeof(line), stdin)) {
+      std::cout << '\n';
+      break;
+    }
+
+    std::cout << line << '\n';
+  }
 }
 
 void runFile() {
+  std::cout << "runFile()\n"; // Temp
 }
 
 int main(int argc, char *argv[]) {
-    if (argc == 0) {
-        repl();
-    } else if (argc == 1) {
-        runFile();
-    }
+  if (argc == 1) {
+    repl();
+  } else if (argc == 2) {
+    runFile();
+  } else {
+    // Print an error, telling the user the expected usage
+    // Exit w/ an exit code
+  }
 
-    return 0;
+  return 0;
 }
