83e43d33b5012eafce6613c741e875b8d060413f
Caleb Owusu-Yianoma <caleb_owusuyianoma@hotmail.co.uk>
2020-11-17 12:25:23 +0000

    Refactor ClosureObject

--- Tone/closureobject.cpp
+++ Tone/closureobject.cpp
@@ -1,26 +1,21 @@
 #include "closureobject.h"
 
-ClosureObject::ClosureObject() : Object{ OBJECT_CLOSURE } {}
-
-ClosureObject::ClosureObject(std::shared_ptr<FunctionObject> function) : Object{ OBJECT_CLOSURE } {
-  this->function = std::move(function); // Q: is there a better way to do assign/initialise here?
+ClosureObject::ClosureObject(std::shared_ptr<FunctionObject> function) :
+  Object{ OBJECT_CLOSURE }, function(std::move(function))
+{
 }
 
-//void ClosureObject::setUpvalue(int index, UpvalueObject* upvalue) {
-void ClosureObject::setUpvalue(int index, std::shared_ptr<UpvalueObject> upvalue) {
-  if (index < 0) {
-    return; // Q: ok for now. What's the best thing to do long-term?
-  } else if (index >= (int)upvalues.size()) { // Q: is the cast ok?
+void ClosureObject::setUpvalue(int index,
+                               std::shared_ptr<UpvalueObject> upvalue) {
+
+  if (index >= static_cast<int>(upvalues.size())) {
     upvalues.push_back(std::move(upvalue));
   } else {
     upvalues.at(index) = std::move(upvalue);
   }
 }
 
-//UpvalueObject* ClosureObject::getUpvalue(int index) {
 std::shared_ptr<UpvalueObject> ClosureObject::getUpvalue(int index) {
-  // Q: error handling?
-
   return upvalues.at(index);
 }
 
