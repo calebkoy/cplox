b81e18a740b5ca2a5a99b7fe0f4b66db741f64ca
Caleb Owusu-Yianoma <caleb_owusuyianoma@hotmail.co.uk>
2020-11-17 11:05:38 +0000

    Refactor bound method object class

--- Tone/boundmethodobject.cpp
+++ Tone/boundmethodobject.cpp
@@ -2,16 +2,12 @@
 
 #include <utility>
 
-BoundMethodObject::BoundMethodObject() : Object{ OBJECT_BOUND_METHOD } {}
-
-BoundMethodObject::BoundMethodObject(Value receiver, std::shared_ptr<ClosureObject> method) :
-  Object{ OBJECT_BOUND_METHOD }
+BoundMethodObject::BoundMethodObject(Value receiver,
+                                     std::shared_ptr<ClosureObject> method) :
+  Object{ OBJECT_BOUND_METHOD }, receiver{ receiver }, method(std::move(method))
 {
-  this->receiver = receiver;
-  this->method = std::move(method);
 }
 
-//ClosureObject* BoundMethodObject::getMethod() {
 std::shared_ptr<ClosureObject> BoundMethodObject::getMethod() {
   return method;
 }
--- Tone/boundmethodobject.h
+++ Tone/boundmethodobject.h
@@ -9,16 +9,11 @@
 
 class BoundMethodObject : public Object {
   Value receiver;
-  //ClosureObject* method; // Q: should this be a smart pointer?
   std::shared_ptr<ClosureObject> method;
 
-
 public:
-  BoundMethodObject(); // Q: is a default ctor needed?
-  //BoundMethodObject(Value receiver, ClosureObject* method);
   BoundMethodObject(Value receiver, std::shared_ptr<ClosureObject> method);
 
-  //ClosureObject* getMethod();
   std::shared_ptr<ClosureObject> getMethod();
   Value getReceiver();
 };
